c6e0910d861f4978132c88012f032d01ea408f8a
PavelFalta <paja.falta@gmail.com>
2025-02-25 16:36:43 +0000

    Rename and refactor functions for binary encoding and decoding, and update their usage in the minf function

--- EMO/cv1.py
+++ EMO/cv1.py
@@ -4,8 +4,7 @@ import numpy as np
 def kvadraticka_funkce(a, b, c, x):
     return a * x ** 2 + b * x - c # s minusem
 
-def gama(n, k):
-    cisla = [random.randint(1, 100) for _ in range(n)]
+def binary_kod(cisla, k):
     m = max(cisla)
     bin_len_m = len(bin(m)) - 2
 
@@ -13,10 +12,9 @@ def gama(n, k):
 
     bin_len_m = max(bin_len_m, k)
     cisla = [bin(i)[2:].zfill(bin_len_m) for i in cisla]
-    print(cisla)
     return "".join(cisla)
 
-def opak_gama(s, k):
+def gama(s, k):
     bin_len_m = k
     cisla = [s[i:i+bin_len_m] for i in range(0, len(s), bin_len_m)]
     cisla = [int(i, 2) for i in cisla]
@@ -27,8 +25,10 @@ def minf(i_max):
 
     for i in range(i_max):
 
-        x = gama(4, 10)
-        der_x = opak_gama(x, 10)
+        cisla = [random.randint(1, 100) for _ in range(4)]
+
+        x = binary_kod(cisla, 10)
+        der_x = gama(x, 10)
 
         a = der_x[0]
         b = der_x[1]
