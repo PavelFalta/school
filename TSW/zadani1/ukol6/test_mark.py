21aa96385fec5596bdac98361e894907a5d794f3
PavelFalta <paja.falta@gmail.com>
2025-02-25 10:38:34 +0000

    Add unit tests for the Calculator class and remove pytest.ini file

---
+++ TSW/zadani1/ukol6/test_mark.py
@@ -0,0 +1,12 @@
+from calculator import Calculator
+import pytest
+
+@pytest.fixture
+def calculator():
+    return Calculator()
+
+@pytest.mark.addition
+def test_add(calculator):
+    assert calculator.add(2, 3) == 5
+    assert calculator.add(-1, 1) == 0
+
