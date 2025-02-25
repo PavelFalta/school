8b111f510d6c4395648ba90d1341359b00e8663b
PavelFalta <paja.falta@gmail.com>
2025-02-25 10:27:01 +0000

    Update age validation logic and corresponding test cases to check if age is between 18 and 65

--- TSW/zadani1/ukol5/test_valid_age.py
+++ TSW/zadani1/ukol5/test_valid_age.py
@@ -1,12 +1,9 @@
 
 
 def is_valid_age(age):
-    return 0 <= age <= 120
+    return 18 <= age <= 65
 
-
-assert is_valid_age(-1) == False
-assert is_valid_age(0) == True
-assert is_valid_age(1) == True
-assert is_valid_age(119) == True
-assert is_valid_age(120) == True
-assert is_valid_age(121) == False
+assert is_valid_age(17) == False
+assert is_valid_age(18) == True
+assert is_valid_age(65) == True
+assert is_valid_age(66) == False
