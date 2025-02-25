66e8aac915d0706e26d927eda88a4f66f3552fde
PavelFalta <paja.falta@gmail.com>
2025-02-25 16:22:43 +0000

    Replace blind_algo function with kvadraticka_funkce and update gama function to generate and print binary encoded random numbers

--- EMO/cv1.py
+++ EMO/cv1.py
@@ -1,16 +1,17 @@
 import random
 
-def blind_algo(tmax):
-    minf = float('inf')
-    for i in range(tmax):
-        x = f"{random.randint(0, 999999999):b}"
-        if gama(x) < minf:
-            minf = gama(x)
-    return minf
+def kvadraticka_funkce(a, b, c, x):
+    return a * x ** 2 + b * x - c # s minusem
+
+def gama(n, k):
+    cisla = [random.randint(1, 100) for _ in range(n)]
+    m = max(cisla)
+    bin_len_m = len(bin(m)) - 2
+    assert bin_len_m <= k, "Nelze zakodovat"
+    bin_len_m = max(bin_len_m, k)
+    cisla = [bin(i)[2:].zfill(bin_len_m) for i in cisla]
+    print("".join(cisla))
 
 
-def gama(tn):
-    return int(tn, 2)
+gama(3, 6)
 
-
-print(blind_algo(1000000))
