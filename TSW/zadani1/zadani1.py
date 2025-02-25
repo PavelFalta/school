e55623a2b57f6c9788e11ed73f0897984e5cd03e
PavelFalta <paja.falta@gmail.com>
2025-02-25 09:45:36 +0000

    Remove comments from test class and methods in zadani1.py

--- TSW/zadani1/zadani1.py
+++ TSW/zadani1/zadani1.py
@@ -2,12 +2,12 @@ from calculator import Calculator
 import unittest
 
 
-class TestCalculator(unittest.TestCase): # testovací třída
+class TestCalculator(unittest.TestCase): 
     def setUp(self):
         self.calc = Calculator()
 
-    def test_add(self): # testovací funkce/metoda
-        self.assertEqual(self.calc.add(2, 3), 5) # testovací případ
+    def test_add(self): 
+        self.assertEqual(self.calc.add(2, 3), 5) 
         self.assertEqual(self.calc.add(-1, 1), 0)
 
     def test_subtract(self):
