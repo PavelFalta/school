de8b968e5239749fd8c320a2f89bb28cad4bf501
PavelFalta <paja.falta@gmail.com>
2025-02-25 10:15:58 +0000

    Modify test setup to use pytest fixture and correct expected result in parameterized test

--- TSW/zadani1/ukol4/aadd_test.py
+++ TSW/zadani1/ukol4/aadd_test.py
@@ -1,7 +1,10 @@
 import pytest
 from calculator import Calculator
 
-calculator = Calculator()
+@pytest.fixture
+def calculator():
+    return Calculator()
+
 def test_add(a, b):
     return calculator.add(a, b)
 
@@ -9,7 +12,7 @@ def test_add(a, b):
     pytest.param(1, 2, 3, id="positive integers"),
     pytest.param(-1, -1, -2, id="negative integers"),
     pytest.param(0, 0, 0, id="zero"),
-    pytest.param(-1, 1, 0, id="negative and positive integers"),
+    pytest.param(-1, 1, 10, id="negative and positive integers"),
 ])
 def test_addd(a, b, result):
     assert test_add(a, b) == result
