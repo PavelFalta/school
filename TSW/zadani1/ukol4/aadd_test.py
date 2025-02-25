30ce29e75396e1fa179fda5585e6ab0e8d7b3451
PavelFalta <paja.falta@gmail.com>
2025-02-25 10:12:44 +0000

    Fix typos in test function names and correct test case result

--- TSW/zadani1/ukol4/aadd_test.py
+++ TSW/zadani1/ukol4/aadd_test.py
@@ -5,15 +5,14 @@ from calculator import Calculator
 def calculator():
     return Calculator()
 
-def def_test_add(calculator):
-    assert calculator.add(1, 2) == 3
-    assert calculator.add(0, 0) == 0
+def test_add(a, b, result):
+    assert calculator.add(a, b) == result
 
 @pytest.mark.parametrize("a, b, result", [
     pytest.param(1, 2, 3, id="positive integers"),
     pytest.param(-1, -1, -2, id="negative integers"),
     pytest.param(0, 0, 0, id="zero"),
-    pytest.param(-1, 1, 0, id="negative and positive integers"),
+    pytest.param(-1, 1, 10, id="negative and positive integers"),
 ])
-def par_test_add(a, b, result):
-    assert def_test_add(a, b) == result
+def test_addd(a, b, result):
+    assert test_add(a, b) == result
