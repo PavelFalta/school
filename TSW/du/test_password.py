b7de62ce62d7ea6217fa1108ad9911fae83cf56c
PavelFalta <paja.falta@gmail.com>
2025-03-03 19:41:58 +0000

    Add unit tests for password validation function using pytest

---
+++ TSW/du/test_password.py
@@ -0,0 +1,14 @@
+from password import is_valid_password
+import pytest
+
+@pytest.fixture
+def fixture():
+    return is_valid_password()
+
+
+@pytest.mark.parametrize("password, result", [
+    pytest.param("Aa123456789", True, id="valid password"),
+
+])
+def test_add(is_valid_password, password, result):
+    assert is_valid_password(password) == result
