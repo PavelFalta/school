a68edad5cf1635d94b76c650d83d4d5829c15255
PavelFalta <paja.falta@gmail.com>
2025-03-03 19:32:35 +0000

    Add password validation functions for length, numeric, and uppercase checks

--- TSW/du/password.py
+++ TSW/du/password.py
@@ -1,2 +1,33 @@
+def len_check(password:str, minlen:int):
+    if len(password) < minlen: 
+        return False
+    return True
+
+def numeric_check(password:str, minfreq:int):
+    count = 0
+
+    for char in password:
+        if char.isdigit():
+            count += 1
+        if count >= minfreq:
+            return True
+    
+    return False
+
+def upper_check(password:str, minfreq:int):
+    count = 0
+    
+    for char in password:
+        if char.isdigit():
+            count += 1
+        if count >= minfreq:
+            return True
+    
+    return False
+
+def lower_check(password:str):
+    ...
+    
+
 def is_valid_password(password:str) -> bool:
     ...
