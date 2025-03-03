1877686beb75923df694f5e32a27ff4bc7e9f0ec
PavelFalta <paja.falta@gmail.com>
2025-03-03 20:52:28 +0000

    Add additional test cases for password validation

--- TSW/du/test_password.py
+++ TSW/du/test_password.py
@@ -7,6 +7,11 @@ def valid_password_checker():
 
 @pytest.mark.parametrize("password, result", [
     pytest.param("Aa123456789", True, id="valid password"),
+    pytest.param("aaa", False, id="too short"),
+    pytest.param("aaaaaaaaaaaaaaaaaaa", False, id="missing num"),
+    pytest.param("aaaaaaa6aaaaaaaaaaaa", False, id="missing upper"),
+    pytest.param("AAAAAAAAAAAAAAAAAA666", False, id="missing lower"),
+    pytest.param("66666666666666666666666666", False, id="missing alpha"),
 ])
 def test_add(valid_password_checker, password, result):
     assert valid_password_checker(password) == result
