7b2c9f8da4eb18dbee556f77ae1d3d3ac173122e
PavelFalta <paja.falta@gmail.com>
2025-03-04 10:20:35 +0000

    Add balance management methods to BankAccount class and new unit tests for deposit and initial balance

--- TSW/zadani2/cv3.py
+++ TSW/zadani2/cv3.py
@@ -8,14 +8,14 @@ class Kalkulacka:
 
 class BankAccount:
     def __init__(self):
-        ...
+        self.balance = 0.0
 
     def deposit(self, amount):
-        ...
+        self.balance += amount
     
     def withdraw(self, amount):
         ...
     
     def get_balance(self):
-        ...
+        return self.balance
     
--- TSW/zadani2/test_bdd.py
+++ TSW/zadani2/test_bdd.py
@@ -14,3 +14,19 @@ def test_deposit(acc):
     outcome = acc.get_balance()
 
     assert expected_balance == outcome
+
+def test_minus_deposit(acc):
+    amount = -100
+    expected_balance = -100
+
+    acc.deposit(amount)
+    outcome = acc.get_balance()
+
+    assert expected_balance == outcome
+
+def test_initial_balance(acc):
+    expected_balance = 0
+
+    outcome = acc.get_balance()
+
+    assert expected_balance == outcome
