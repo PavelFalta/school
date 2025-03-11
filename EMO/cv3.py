454ff2b79b5ac656596ff1b3a293f014e7a61c07
PavelFalta <paja.falta@gmail.com>
2025-03-11 16:04:07 +0000

    Add tabu search algorithm and utility functions for genetic operations

--- EMO/cv3.py
+++ EMO/cv3.py
@@ -1,14 +1,43 @@
 from collections import deque
 from random import randint
-
+from random import random
 
 def negace(gen, i):
     gen[i] = 1 - gen[i]
     return gen
 
-def U(alpha, S, T):
-    return [S(alpha) for i in S if i not in T]
+def U(alpha, S, tabu):
+    return [negace(alpha, i) for i in S if i not in tabu]
 
-gen = [0, 1, 0, 1, 1, 0, 1, 0, 1, 0]
-gen = negace(gen, 3)
-print(gen)
+def random_alpha(size):
+    return ''.join(random.choice('01') for _ in range(size))
+
+def gamma(alpha, k, n):
+    return [gray_dec(alpha[i*k:(i+1)*k]) for i in range(n)]
+
+def gray_dec(alpha):
+    
+    binary = int(alpha[0])
+    result = binary
+    for bit in alpha[1:]:
+        binary ^= int(bit)
+        result = (result << 1) | binary
+    return result
+
+def tabu_search(f, tmax, k, n, c0, pmut, S, T):
+    f_min = float("inf")
+    alpha_min = None
+    alpha = random_alpha(size=k*n)
+    tabu = deque(maxlen=T)
+    for time in range(tmax):
+        U_ = U(alpha, S, tabu)
+        U_ = [mutation(alpha, pmut) for i in range(c0)]
+        alpha = min(U_, key=lambda x: f(gamma(x, k, n)))
+        x = gamma(alpha, k, n)
+        f_x = f(x)
+        if f_x < f_min:
+            f_min = f_x
+            alpha_min = alpha
+        tabu.append(alpha)
+        print(f"t: {time}, alpha: {alpha}, x: {x}, f(x): {f_x}, f_min: {f_min}")
+    return (alpha_min, f_min)
