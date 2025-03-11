898688c451f0eba4328aac1eed1966d53907609d
PavelFalta <paja.falta@gmail.com>
2025-03-11 16:18:53 +0000

    Update tabu search to append the best index to the tabu list and add a comment for clarity

--- EMO/cv3.py
+++ EMO/cv3.py
@@ -44,8 +44,9 @@ def tabu_search(f, tmax, k, n, maxlen):
         if current_f < f_min:
             f_min = current_f
 
-        tabu.append(alpha_min)
-
+        # Append the best i to tabu
+        best_i = S[_U.index(alpha_min)]
+        tabu.append(best_i)
 
         print(f"t: {t}, alpha: {alpha_min}, x: {x}, f(x): {current_f}, f_min: {f_min}, tabu: {tabu}")
     return (alpha_min, f_min)
