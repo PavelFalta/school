69dab1c6a4b0bfda59178a2e85d46384792af9a9
PavelFalta <paja.falta@gmail.com>
2025-03-11 16:09:09 +0000

    Simplify tabu_search function by removing unnecessary variables and updating parameters

--- EMO/cv3.py
+++ EMO/cv3.py
@@ -28,19 +28,13 @@ def gray_dec(alpha):
 def tabu_search(f, tmax, k, n, c0, pmut, S, T):
     f_min = float("inf")
     alpha_min = None
-    alpha = random_alpha(size=k*n)
     tabu = deque(maxlen=T)
+
     for t in range(tmax):
-        U_ = U(alpha, S, tabu)
-        alpha = min(U_, key=lambda x: f(gamma(x, k, n)))
-        x = gamma(alpha, k, n)
-        f_x = f(x)
-        if f_x < f_min:
-            f_min = f_x
-            alpha_min = alpha
-        tabu.append(alpha)
-        print(tabu)
+        alpha = random_alpha(size=k*n)
+        print(gamma(alpha, k,n))
+        
         #print(f"t: {time}, alpha: {alpha}, x: {x}, f(x): {f_x}, f_min: {f_min}")
     return (alpha_min, f_min)
 
-tabu_search(f = lambda x: -x[0]**2 + 6, tmax = 30, k = 4, n = 1, c0=10, pmut=0.1, S=[0, 1, 2, 3], T=10)
+tabu_search(f = lambda x: -x[0]**2 + 6, tmax = 30, k = 4, n = 5, c0=10, pmut=0.1, S=[0, 1, 2, 3], T=10)
