e8c9c103782132bf74841949b5e95ee1a237f949
PavelFalta <paja.falta@gmail.com>
2025-03-11 09:46:54 +0000

    Refactor code to load environment variables within the main block and remove redundant comments

--- TSW/zadani3/externi.py
+++ TSW/zadani3/externi.py
@@ -4,19 +4,16 @@ from logger import WeatherDataLogger
 from weather_stub import WeatherService
 
 
-# Načtení proměnných z .env souboru
-load_dotenv()
-API_KEY = os.getenv("API_KEY")
-
-
-
-# Testovací běh
 if __name__ == "__main__":
+    load_dotenv()
+
+    API_KEY = os.getenv("API_KEY")
     city = "Prague"
+
     service = WeatherService(API_KEY=API_KEY)
     response = service.get_weather(city)
+    
     temperature = response["main"]["temp"]
-    print(temperature)
 
     logger = WeatherDataLogger()
     logger.update_data(city, temperature)
