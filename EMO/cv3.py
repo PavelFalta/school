a6b74db1829778c1bbde83c7a76d219f812e0e69
PavelFalta <paja.falta@gmail.com>
2025-03-11 15:48:38 +0000

    Add utility functions for random negation and updating sequences using negation

--- EMO/cv3.py
+++ EMO/cv3.py
@@ -1,6 +1,13 @@
+from collections import deque
+from random import randint
+
+
 def negace(gen, i):
     gen[i] = 1 - gen[i]
     return gen
+
+def U(alpha, S):
+    return [negace(i, randint(0, len(i))) for i in S]
 
 gen = [0, 1, 0, 1, 1, 0, 1, 0, 1, 0]
 gen = negace(gen, 3)
