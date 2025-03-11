ef92916c44192f2d6294ca98e0c0a5c914897c6d
PavelFalta <paja.falta@gmail.com>
2025-03-11 09:52:26 +0000

    Update weather_mock.py to count and print the number of zeros in the binary representation of the city name

--- TSW/zadani3/weather_mock.py
+++ TSW/zadani3/weather_mock.py
@@ -13,8 +13,11 @@ class WeatherService:
             raise ValueError("API klíč není nastaven.")
         
         city_binary = ' '.join(format(ord(char), 'b') for char in city)
+
         ones_count = city_binary.count('1')
-        print(f"{city_binary} (number of 1s: {ones_count})")
+        zeros_count = city_binary.count('0')
+
+        print(f"{ones_count}")
 
         return {
             'coord': {'lon': 14.4208, 'lat': 50.088},
