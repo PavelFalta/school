55eda4cce1dddd517326d1c258b63c53e4e64ff3
PavelFalta <paja.falta@gmail.com>
2025-03-12 13:40:56 +0000

    Add data scaling using StandardScaler and update visualization with title

--- USU/cv4.py
+++ USU/cv4.py
@@ -1,15 +1,25 @@
 from cv3 import naivni_logisticka_regrese_binarni
 from sklearn.datasets import load_digits
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
 
 #visualize data
 digits = load_digits()
+
+
 plt.gray()
 plt.matshow(digits.images[3])
-print(digits.images[3])
-#print label
-print(digits.target[3])
+plt.title(digits.target[3])
 plt.show()
+
+#scale data
+scaler = StandardScaler()
+scaler.fit(digits.data)
+
+X = scaler.transform(digits.data)
+Y = digits.target
+
+print(X[0])
 
 # try to clasify one number from rest
