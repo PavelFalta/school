9f1a61f88ac1257cf28f79910fa6853d1794457c
PavelFalta <paja.falta@gmail.com>
2025-03-12 13:37:51 +0000

    Add data visualization and classification attempt for digits dataset

--- USU/cv4.py
+++ USU/cv4.py
@@ -1,2 +1,13 @@
 from cv3 import naivni_logisticka_regrese_binarni
+from sklearn.datasets import load_digits
+import matplotlib.pyplot as plt
 
+
+#visualize data
+digits = load_digits()
+plt.gray()
+plt.matshow(digits.images[0])
+print(digits.images[0])
+plt.show()
+
+# try to clasify one number from rest
