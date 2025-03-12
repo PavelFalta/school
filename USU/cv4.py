df0e5b85e67735aee9fecaea6080633c6b69ee5b
PavelFalta <paja.falta@gmail.com>
2025-03-12 13:53:45 +0000

    Update logistic regression to handle numerical stability, switch scaler to MinMaxScaler, replace LogisticRegression with custom binary logistic regression, and add MultiModalRegression class

--- USU/cv4.py
+++ USU/cv4.py
@@ -1,11 +1,12 @@
 from cv3 import naivni_logisticka_regrese_binarni
 from sklearn.datasets import load_digits
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 from sklearn.linear_model import LogisticRegression
 from collections import Counter
 from sklearn.model_selection import train_test_split
+import numpy as np
 
 #visualize data
 digits = load_digits()
@@ -16,7 +17,7 @@ digits = load_digits()
 # plt.title(digits.target[3])
 # plt.show()
 
-scaler = StandardScaler()
+scaler = MinMaxScaler()
 scaler.fit(digits.data)
 
 X = scaler.transform(digits.data)
@@ -27,7 +28,7 @@ Y = 1 * (Y == 3)
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
 
-model = LogisticRegression()
+model = naivni_logisticka_regrese_binarni()
 model.fit(X_train, Y_train)
 
 Y_pred = model.predict(X_test)
@@ -42,3 +43,27 @@ print(cm)
 
 report = classification_report(Y_test, Y_pred)
 print(report)
+
+class MultiModalRegression:
+    def __init__(self, models):
+        self.models = models
+
+    def fit(self, X, Y):
+        for model in self.models:
+            model.fit(X, Y)
+
+    def predict(self, X):
+        predictions = []
+        for model in self.models:
+            predictions.append(model.predict(X))
+
+        predictions = np.array(predictions)
+        return np.argmax(predictions, axis=0)
+
+    def predict_proba(self, X):
+        predictions = []
+        for model in self.models:
+            predictions.append(model.predict_proba(X))
+
+        predictions = np.array(predictions)
+        return np.argmax(predictions, axis=0)
