d63e1985f5e34eb218b1ef9b605b72e5d499cae1
PavelFalta <paja.falta@gmail.com>
2025-03-19 14:53:08 +0000

    Remove unused cv.6.py file

---
+++ USU/cv6.py
@@ -0,0 +1,22 @@
+#load from lomy, two folders with images, two classes
+
+import os
+import cv2
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        img = cv2.imread(os.path.join(folder,filename))
+        if img is not None:
+            images.append(img)
+    return images
+
+folder1 = 'lomy/stepnylom_jpg'
+folder2 = 'lomy/tvarnylom_jpg'
+
+images1 = load_images_from_folder(folder1)
+images2 = load_images_from_folder(folder2)
+print(len(images1))
+print(len(images2))
