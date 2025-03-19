d36b4a14b5a20ab4421bed3f0535035fda8ca5fe
PavelFalta <paja.falta@gmail.com>
2025-03-19 14:58:12 +0000

    Add color histogram generation and display functionality using matplotlib for image datasets

--- USU/cv6.py
+++ USU/cv6.py
@@ -4,6 +4,7 @@ import os
 import cv2
 import numpy as np
 from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
 
 def load_images_from_folder(folder):
     images = []
@@ -18,5 +19,23 @@ folder2 = 'lomy/tvarnylom_jpg'
 
 images1 = load_images_from_folder(folder1)
 images2 = load_images_from_folder(folder2)
-print(len(images1))
-print(len(images2))
+# for both classes, get colors and display color histograms
+
+def get_colors(images):
+    colors = []
+    for image in images:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        for i in range(3):
+            colors.append(image[:, :, i].ravel())
+    return colors
+
+colors1 = get_colors(images1)
+colors2 = get_colors(images2)
+
+def display_histograms(colors):
+    for i in range(len(colors)):
+        plt.hist(colors[i], bins=256, range=(0, 256), density=True, alpha=0.5)
+    plt.show()
+
+display_histograms(colors1)
+display_histograms(colors2)
