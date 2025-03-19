83b97eef7549454cd8dfc4fc8e1814aa3e6d06fa
PavelFalta <paja.falta@gmail.com>
2025-03-19 14:59:41 +0000

    Improve image loading performance by parallelizing file reading with ThreadPoolExecutor

--- USU/cv6.py
+++ USU/cv6.py
@@ -6,13 +6,20 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
 
+import concurrent.futures
+
 def load_images_from_folder(folder):
+    def load_image(filepath):
+        img = cv2.imread(filepath, cv2.IMREAD_UNCHANGED)
+        return img
+
     images = []
     filepaths = [os.path.join(folder, filename) for filename in os.listdir(folder)]
-    for filepath in filepaths:
-        img = cv2.imread(filepath, cv2.IMREAD_UNCHANGED)
-        if img is not None:
-            images.append(img)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = executor.map(load_image, filepaths)
+        for img in results:
+            if img is not None:
+                images.append(img)
     return images
 
 folder1 = 'lomy/stepnylom_jpg'
@@ -20,6 +27,7 @@ folder2 = 'lomy/tvarnylom_jpg'
 
 images1 = load_images_from_folder(folder1)
 images2 = load_images_from_folder(folder2)
+print(len(images1), len(images2))
 # for both classes, get colors and display color histograms
 
 def get_colors(images):
