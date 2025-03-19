b6aee8de0998f23501197fd5f73963204802c4bd
PavelFalta <paja.falta@gmail.com>
2025-03-19 14:55:37 +0000

    Update image loading function to use OpenCV instead of NumPy for reading images

--- USU/cv6.py
+++ USU/cv6.py
@@ -1,13 +1,14 @@
 #load from lomy, two folders with images, two classes
 
 import os
+import cv2
 import numpy as np
 from sklearn.model_selection import train_test_split
 
 def load_images_from_folder(folder):
     images = []
     for filename in os.listdir(folder):
-        img = np.loadtxt(os.path.join(folder, filename))
+        img = cv2.imread(os.path.join(folder,filename))
         if img is not None:
             images.append(img)
     return images
