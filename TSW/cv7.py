74ffd6fddfe92a326fad754f1b8da7088955ce5a
PavelFalta <paja.falta@gmail.com>
2025-04-01 08:36:18 +0000

    Add placeholder file for future implementation

---
+++ TSW/cv7.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import dotenv
+import os
+
+dotenv.load_dotenv()
+driver = webdriver.Chrome()
+
+username = os.getenv("STAG_USERNAME")
+password = os.getenv("STAG_PASSWORD")
+
+try:
+    
+    driver.get("https://portal.ujep.cz/")
+    
+    username_input = driver.find_element(By.NAME, "loginName")
+    password_input = driver.find_element(By.NAME, "password")
+    
+    username_input.send_keys(username)
+    password_input.send_keys(password)
+    
+    password_input.send_keys(Keys.RETURN)
+    
+    time.sleep(1)
+    
+    if "přihlášen" not in driver.page_source:
+        raise Exception("Přihlášení selhalo.")
+    print("Test přihlášení prošel.")
+except Exception as e:
+    print("Test selhal:", str(e))
+finally:
+    driver.quit()
