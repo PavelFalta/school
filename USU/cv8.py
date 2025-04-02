3abd41a0c07cbf56d4308a9fe00b8fd5a8f41b05
PavelFalta <paja.falta@gmail.com>
2025-04-02 13:22:52 +0000

    Apr 2, 2025, 3:22 PM

--- USU/cv8.py
+++ USU/cv8.py
@@ -5,3 +5,14 @@ import pandas as pd
 df = pd.read_csv(path)
 
 print(df.head())
+
+columns = df.columns
+
+target = "kp0"
+
+for col in columns:
+    if target in col:
+        print(col)
+
+# 1. 
+
