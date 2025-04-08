6953e58754716845b968906578ddd4ea16ed9a2c
PavelFalta <paja.falta@gmail.com>
2025-04-08 08:20:25 +0000

    Update script to perform drag-and-drop action on a specific webpage using Selenium

--- TSW/cv8.py
+++ TSW/cv8.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver import ActionChains
 
 
 driver = webdriver.Chrome()
-driver.get("https://example.com")
+driver.get("https://jqueryui.com/droppable/")
+
+driver.switch_to.frame(0)
+
+ActionChains(driver).drag_and_drop(source, target).perform()
 
 
 assert driver.find_element(By.TAG_NAME, "h1").text == "Example Domain"
