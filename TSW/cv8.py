6cbac4e26cdc50eeeeb5b3da57af6298ae871b0f
PavelFalta <paja.falta@gmail.com>
2025-04-08 08:17:20 +0000

    Add initial empty Python file

---
+++ TSW/cv8.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import dotenv
+import os
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from time import perf_counter
