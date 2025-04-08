e30e2e214360dfc35409a178a99591444ad82993
PavelFalta <paja.falta@gmail.com>
2025-04-08 08:35:29 +0000

    Add print statement to output the page source after scrolling

--- TSW/cv8.py
+++ TSW/cv8.py
@@ -34,6 +34,8 @@ print(len(driver.find_elements(By.TAG_NAME, "article")))
 
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
+print(driver.page_source)
+
 time.sleep(1)
 
 print(len(driver.find_elements(By.TAG_NAME, "article")))
