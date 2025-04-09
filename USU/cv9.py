5c54c34836a91127ea3b6ec81a24250b68d58c8b
PavelFalta <paja.falta@gmail.com>
2025-04-09 13:04:39 +0000

    Apr 9, 2025, 3:04 PM

--- USU/cv9.py
+++ USU/cv9.py
@@ -3,7 +3,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 x = np.linspace(0, 10, 100)
-y = x + np.random.normal(size=100)
+y = x + np.random.normal(size=100) * 2
 
 plt.scatter(x, y)
 plt.show()
+
