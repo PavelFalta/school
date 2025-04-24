d149f0c65ee506b22662c26bd756e1f77a6f972e
PavelFalta <paja.falta@gmail.com>
2025-04-24 08:53:57 +0000

    Apr 24, 2025, 10:53 AM

--- USU/cv8.py
+++ USU/cv8.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import mean_squared_error
 from sklearn.ensemble import RandomForestRegressor
 import re
 import os
