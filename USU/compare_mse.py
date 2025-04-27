d2eed98cc0be335c663c7feab946f5fae5688d72
PavelFalta <paja.falta@gmail.com>
2025-04-27 16:41:05 +0000

    Apr 27, 2025, 6:41 PM

--- USU/compare_mse.py
+++ USU/compare_mse.py
@@ -1,6 +1,6 @@
 path_jara = "data/predikce_jara"
 path_pavel = "data/predikce_pavel"
 
-
-
-
+# csv colums: skutecna_kp0_y,skutecna_kp0_x,nejvetsi_vaha_kp0_y,nejvetsi_vaha_kp0_x,predikce_kp0_y,predikce_kp0_x
+# from 0 to 23
+# udelat MSE v X, MSE v Y a soucet MSE X+Y
