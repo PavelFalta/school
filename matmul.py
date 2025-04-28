56e853eadc974f5634dbfb4c59949d388b46670b
PavelFalta <paja.falta@gmail.com>
2025-04-28 10:04:55 +0000

    Update matrix multiplication logic, modify input matrices, add shape assertion, and improve result computation

--- matmul.py
+++ matmul.py
@@ -1,14 +1,16 @@
 import numpy as np
 
 A = np.array(
-    [[1,2,3],
-     [4,5,6],
-     [7,8,9]])
+    [[1,1,1],
+     [1,1,1],
+     [1,1,1]])
 
 B = np.array(
-    [[1],
-     [4],
-     [7]])
+    [[1,2],
+     [4,2],
+     [7,2]])
+
+# 1 + 4 + 7
 
 def printmat(A):
     for row in A:
@@ -20,25 +22,29 @@ def transpose(A):
     return np.array([list(row) for row in list(zip(*A))])
 
 def matmul(A, B):
-    A = transpose(A)
-
+    assert A.shape[1] == B.shape[0], "Youre stupid"
     result_shape = A.shape[0], B.shape[1]
     result = np.zeros(result_shape)
+
+    B = transpose(B)
+
     printmat(result)
 
-    for row_B in B:
-        for row_A in A:
+    for rrow_i, res_row in enumerate(result):
+        for ccol_i, _ in enumerate(res_row):
+            r = []
+            for A_i,item_A in enumerate(A[rrow_i]):
+                r.append(item_A*B[ccol_i, A_i])
+            
+            result[rrow_i, ccol_i] = np.sum(np.array(r))
 
-            for item_B in row_B:
-                for item_A in row_A:
-                    ...
+    
+    return result
 
 
 
-
-printmat(transpose(B))
-matmul(A, B)
+printmat(matmul(A, B))
 
 A = np.array(A)
 B = np.array(B)
-print(A @ B)
+printmat(A @ B)
